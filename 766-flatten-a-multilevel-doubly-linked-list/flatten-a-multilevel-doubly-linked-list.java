e9a621fd00fa95306f2bc9d691862eb779e8106d
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2025-10-31 12:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (59.99%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        Node temp=head;
+        Stack<Node> st=new Stack<>();
+        while(temp!=null)//from 1 to 12
+        {
+            if(temp.child!=null){//we check if child is not null we connect
+                if(temp.next!=null)st.push(temp.next);
+                temp.next=temp.child;
+                temp.child=null;
+                temp.next.prev=temp;
+                temp=temp.next;
+                continue;
+            }
+            if(!st.isEmpty() && temp.next==null){
+                temp.next=st.pop();
+                temp.next.prev=temp;
+            }
+            temp=temp.next;
+        }
+        return head;
+    }
+}
