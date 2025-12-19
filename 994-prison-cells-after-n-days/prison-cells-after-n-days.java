437105de2615867bc403ab027f5d46c437c99d9d
Soumya Upadhyay <soumyaupadhya121@gmail.com>
2025-12-19 21:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (50.00%) - LeetSync

---
+++ 994-prison-cells-after-n-days/prison-cells-after-n-days.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] prisonAfterNDays(int[] cells, int n) {
+        n = (n - 1) % 14 + 1;
+
+        for (int day = 0; day < n; day++) {
+            int[] next = new int[8];
+
+            for (int i = 1; i < 7; i++) {
+                next[i] = (cells[i - 1] == cells[i + 1]) ? 1 : 0;
+            }
+
+            cells = next;
+        }
+
+        return cells;
+    }
+}
